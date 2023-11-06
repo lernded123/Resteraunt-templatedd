599c61dd9c0bd4b6f97f81eea2e9287140e4ab5d
Sams <goodlifesam@gmail.com>
2023-11-06 15:41:09 -0800

    added menu.html

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //Tabbed Menu
 function openMenu(event, menuName) {
   let menuArray = document.getElementsByClassName("menu");
-  for (i = 0; i < menuArray.length; i++) {
+  for (let i = 0; i < menuArray.length; i++) {
     menuArray[i].style.display = "none";
   }
 
   let tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < menuArray.length; i++) {
+  for (let i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("active-tab");
   }
 
